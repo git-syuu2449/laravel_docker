4747fdde37df4ba8b485a3d8942536202f2302a2
syuu2449 <syuu2449@gmail.com>
2025-04-26 17:04:51 +0900

    UserModelの削除は影響が大きいのでスケルトン状態で追加 Modelのディレクトリ構造を変更

--- laravel-project/database/factories/QuestionFactory.php
+++ laravel-project/database/factories/QuestionFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Question>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Question>
  */
 class QuestionFactory extends Factory
 {
