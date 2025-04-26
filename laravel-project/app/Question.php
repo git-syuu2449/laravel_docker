6a3c78fa21c80113b6cd46a865c8ddccb72670dc
syuu2449 <syuu2449@gmail.com>
2025-04-26 16:49:19 +0900

    stubを追加 Questionモデルを追加 一旦make:modelで作成された雛形を追加

---
+++ laravel-project/app/Question.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    /** @use HasFactory<\Database\Factories\QuestionFactory> */
+    use HasFactory;
+}
