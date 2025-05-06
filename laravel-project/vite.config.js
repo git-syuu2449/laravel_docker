c79a38bd2f00f766c802e044e0d9baf95f64a210
syuu2449 <syuu2449@gmail.com>
2025-05-06 23:34:03 +0900

    vueを使用する準備

--- laravel-project/vite.config.js
+++ laravel-project/vite.config.js
@@ -1,6 +1,7 @@
 
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 import glob from 'glob';
 import path from 'path';
 
@@ -40,5 +41,6 @@ export default defineConfig({
             ],
             refresh: true,
         }),
+        vue(),
     ],
 });
