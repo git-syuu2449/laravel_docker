153b780e818452a3a2e18b99e1983d8ac1b1f581
syuu2449 <syuu2449@gmail.com>
2025-05-14 16:13:17 +0900

    ・バッチ処理のサンプルを追加

--- laravel-project/routes/console.php
+++ laravel-project/routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use App\Console\Commands\SendMail;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// スケジュールの登録
+Schedule::command(SendMail::class, [1, '--opt'])->everyMinute()->withoutOverlapping();
